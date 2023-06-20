bfc94bf48b2cb7f28ec96974bece1f55b57a5e7b
russellbanares322 <banaresrussell11@gmail.com>
2023-06-20 20:25:26 +0800

    feat: implement show password in auth forms

--- src/components/authorization/Login.js
+++ src/components/authorization/Login.js
@@ -1,19 +1,16 @@
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  updateProfile,
-} from "firebase/auth";
-import React from "react";
-import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
 import { Button, Col, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../firebase.config";
 import { toast } from "react-toastify";
+import { HiOutlineEyeOff, HiOutlineEye } from "react-icons/hi";
 import "./Form.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   //Signing up user
@@ -26,6 +23,10 @@ const Login = () => {
       .catch((err) => {
         toast.error(err.message);
       });
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
   };
 
   return (
@@ -41,15 +42,45 @@ const Login = () => {
               onChange={(e) => setEmail(e.target.value)}
             />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group
+            style={{ position: "relative" }}
+            className="mb-3"
+            controlId="formBasicPassword"
+          >
             <Form.Label>Password</Form.Label>
             <Form.Control
-              type="password"
+              type={showPassword ? "text" : "password"}
               placeholder="Enter your password..."
               onChange={(e) => setPassword(e.target.value)}
             />
+            {showPassword ? (
+              <HiOutlineEye
+                onClick={handleShowPassword}
+                color="black"
+                size={22}
+                style={{
+                  position: "absolute",
+                  top: 38,
+                  right: 10,
+                  cursor: "pointer",
+                  display: password.length > 0 ? "block" : "none",
+                }}
+              />
+            ) : (
+              <HiOutlineEyeOff
+                onClick={handleShowPassword}
+                color="black"
+                size={22}
+                style={{
+                  position: "absolute",
+                  top: 38,
+                  right: 10,
+                  cursor: "pointer",
+                  display: password.length > 0 ? "block" : "none",
+                }}
+              />
+            )}
           </Form.Group>
-
           <div className="d-grid gap-2 mt-5 mb-2">
             <Button
               variant="primary"
